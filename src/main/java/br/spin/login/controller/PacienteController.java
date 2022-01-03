3c4aa0f64063943118658ed1510345e62f17d728
IgorSpintec <93929972+IgorSpintec@users.noreply.github.com>
2022-01-03 17:02:35 -0300

    cagando o codigo

--- src/main/java/br/spin/login/controller/PacienteController.java
+++ src/main/java/br/spin/login/controller/PacienteController.java
@@ -1,6 +1,7 @@
 package br.spin.login.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -35,12 +36,11 @@ public class PacienteController {
 	}
 	
 	@PutMapping("/alterar")
-	public PacienteModel alterarPaciente(@RequestBody PacienteModel alterarPaciente) {
-		paciente.getById(alterarPaciente.getId());
-		paciente.save(alterarPaciente);
-		return alterarPaciente;
+	public PacienteModel alterarPaciente(@RequestBody PacienteModel pacienteAlterado) {
+		paciente.save(pacienteAlterado);
+		return pacienteAlterado;
 	}
-	
+
 	@DeleteMapping("/apagar")
 	public PacienteModel deletarPaciente (@RequestBody PacienteModel deletarPaciente) {
 		paciente.deleteById(deletarPaciente.getId());
