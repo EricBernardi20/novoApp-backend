071d74607688efc87e72231832c959d81e6906fe
EricBernardi <eric.bernardi@spintec.com.br>
2022-01-03 08:48:48 -0300

    criação do projeto

---
+++ src/test/java/br/spin/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.spin.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
