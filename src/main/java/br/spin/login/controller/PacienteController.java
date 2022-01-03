f25411930d21eceb1fd782c077087e303323cd49
EricBernardi <eric.bernardi@spintec.com.br>
2022-01-03 16:35:20 -0300

    alterado rota de alterar paciente

--- src/main/java/br/spin/login/controller/PacienteController.java
+++ src/main/java/br/spin/login/controller/PacienteController.java
@@ -1,6 +1,7 @@
 package br.spin.login.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.spin.login.modelos.PacienteModel;
@@ -20,29 +20,33 @@ import br.spin.login.repository.PacienteRepository;
 public class PacienteController {
 	
 	@Autowired
-	private PacienteRepository paciente;
+	private PacienteRepository pacienteRepository;
 	
 	@GetMapping("/listar")
 	public List<PacienteModel> listarPacientes(){
-		List<PacienteModel> lista = paciente.findAll();
+		List<PacienteModel> lista = pacienteRepository.findAll();
 		return lista;
 	}
 	
 	@PostMapping("/adicionar")
 	public PacienteModel adicionarPaciente(@RequestBody PacienteModel adicionarPaciente) {
-		paciente.save(adicionarPaciente);
+		pacienteRepository.save(adicionarPaciente);
 		return adicionarPaciente;
 	}
 	
 	@PutMapping("/alterar")
 	public PacienteModel alterarPaciente(@RequestBody PacienteModel alterarPaciente) {
-		paciente.save(alterarPaciente);
-		return alterarPaciente;
+		Optional<PacienteModel> pacienteExistente = pacienteRepository.findById(alterarPaciente.getId());
+		if(!pacienteExistente.isPresent()) {
+			return null;
+		}
+		PacienteModel pacienteAlterado = pacienteRepository.save(alterarPaciente);
+		return pacienteAlterado;
 	}
 	
 	@DeleteMapping("/apagar")
 	public PacienteModel deletarPaciente (@RequestBody PacienteModel deletarPaciente) {
-		paciente.deleteById(deletarPaciente.getId());
+		pacienteRepository.deleteById(deletarPaciente.getId());
 		return deletarPaciente;
 	}
 }
