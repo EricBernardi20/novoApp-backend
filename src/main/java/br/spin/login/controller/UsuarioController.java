553a1ee1243dec38fab3e936414db8942360a053
EricBernardi <eric.bernardi@spintec.com.br>
2022-01-04 10:43:36 -0300

    Criada a rota de alterar senha do usuario

--- src/main/java/br/spin/login/controller/UsuarioController.java
+++ src/main/java/br/spin/login/controller/UsuarioController.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.spin.login.modelos.PacienteModel;
 import br.spin.login.modelos.UsuarioModel;
 import br.spin.login.repository.UsuarioRepository;
 
@@ -29,5 +31,26 @@ public class UsuarioController {
 		}
 		usuarioRepository.save(criarUsuario);
 		return criarUsuario;
+	}
+
+	@PutMapping("/alterarsenha")
+	public UsuarioModel alterarSenha(@RequestBody UsuarioModel alterarSenha) {
+		Optional<UsuarioModel> userExistingById = usuarioRepository.findById(alterarSenha.getId());
+		if (!userExistingById.isPresent()) {
+			return null;
+		} else {
+			List<UsuarioModel> userExisting = usuarioRepository.findAll();
+			for (int i = 0; i < userExisting.size(); i++) {
+				if (alterarSenha.getSenha().equals(userExisting.get(i).getSenha()) &
+						!alterarSenha.getNome().equals(userExisting.get(i).getNome())) {
+					return null;
+				}
+			}
+
+			alterarSenha.setNome(alterarSenha.getNome());
+		}
+		usuarioRepository.save(alterarSenha);
+		return alterarSenha;
+
 	}
 }
