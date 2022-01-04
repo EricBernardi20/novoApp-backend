319a4abab0d06f1c231647335dd3b37d121c2972
EricBernardi <eric.bernardi@spintec.com.br>
2022-01-04 08:44:33 -0300

    Criada a rota de criar usuários

--- src/main/java/br/spin/login/controller/UsuarioController.java
+++ src/main/java/br/spin/login/controller/UsuarioController.java
@@ -1,11 +1,33 @@
 package br.spin.login.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.spin.login.modelos.UsuarioModel;
+import br.spin.login.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("usuario")
 public class UsuarioController {
 
-	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@PostMapping("/criar")
+	public UsuarioModel criarUsuario(@RequestBody UsuarioModel criarUsuario) {
+		List<UsuarioModel> userEquals = usuarioRepository.findAll();
+		for (int i = 0; i < userEquals.size(); i++) {
+			if (criarUsuario.getNome().equals(userEquals.get(i).getNome())) {
+				return null;
+			}
+		}
+		usuarioRepository.save(criarUsuario);
+		return criarUsuario;
+	}
 }
